9299b770f92a84dc7ec4ec3a9d19fe01231be905
niruthikka24 <niruthikka.19@cse.mrt.ac.lk>
2023-12-30 12:00:17 +0530

    updated routes

--- src/App.js
+++ src/App.js
@@ -7,15 +7,11 @@ import Home from "./Pages/Home/Homescreen";
 function App() {
   return (
     <div className="App">
-      <Router>
-        <div>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="*" element={<div>404 Not Found</div>}></Route>
-          </Routes>
-        </div>
-      </Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="*" element={<div>404 Not Found</div>}></Route>
+      </Routes>
     </div>
   );
 }
